34e1901a608a3206fac59a777c87472a484b0901
ttimbers <tiffany.timbers@gmail.com>
2016-11-21 11:50:32 -0800

    Added package-level documentation

---
+++ R/convertemp-package.r
@@ -0,0 +1,8 @@
+#' convertemp: Easily convert temperatures
+#'
+#' Easily convert temperatures between any combination of Celsius,
+#' Kelvin, and Fahrenheit.
+#'
+#' @name convertemp
+#' @docType package
+NULL
